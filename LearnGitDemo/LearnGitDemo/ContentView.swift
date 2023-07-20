9ccdf25d03f698a61fae3bc452835865cfd9d07c
jindianyzw <jindianyzw@gmail.com>
2023-07-20 10:57:37 +0800

    First Commit

---
+++ LearnGitDemo/LearnGitDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LearnGitDemo
+//
+//  Created by 叶泽伟 on 2023/7/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LearnGitDemo/LearnGitDemo/LearnGitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnGitDemoApp.swift
+//  LearnGitDemo
+//
+//  Created by 叶泽伟 on 2023/7/20.
+//
+
+import SwiftUI
+
+@main
+struct LearnGitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
